5ecce66945b0a253d787a4b30d5d3fddd269132c
kumulynja <b@kumuly.dev>
2023-07-22 16:36:35 +0200

    expose functions from bindings in flutter package

--- packages/unified_mnemonic/lib/unified_mnemonic.dart
+++ packages/unified_mnemonic/lib/unified_mnemonic.dart
@@ -5,6 +5,6 @@ library;
 
 export 'src/ffi.dart';
 
-// TODO: Export any libraries intended for clients of this package.
+// Export any libraries intended for clients of this package.
 export 'package:flutter_rust_bridge/flutter_rust_bridge.dart' show WasmModule;
 export 'src/bridge_generated.dart';
