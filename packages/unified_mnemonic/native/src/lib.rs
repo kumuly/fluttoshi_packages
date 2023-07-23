43304f4a6b114a0609280d4b9cba87044ce71024
kumulynja <b@kumuly.dev>
2023-07-23 22:19:43 +0200

    fix: try to fix actions again

--- packages/unified_mnemonic/native/src/lib.rs
+++ packages/unified_mnemonic/native/src/lib.rs
@@ -1,4 +1,4 @@
-#![recursion_limit = "1048"]
+#![recursion_limit = "2048"]
 
 mod api;
 mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
